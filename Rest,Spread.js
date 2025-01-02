849f964f7e34445b6078cca9c712fd6c6dbf7684
SinghBanta <singhbanta84@gmail.com>
2025-01-02 19:54:30 +0530

    initail commit

---
+++ Rest,Spread.js
@@ -0,0 +1,18 @@
+//Using Rest Operator
+let students = {
+    name: "Ravi",
+    age: 23,
+    hobbies: ["reading", "coding", "playing", "sleeping"],
+};
+
+const {...value} = students; 
+// console.log(value);
+
+// Using spread operator to copy the values of students object.
+let newStudent = {
+    ...students, 
+    name: "Raj", // Changing the name of the new student object.
+    id:100, // Adding new key value pair to the new student object.;
+};
+
+console.log(newStudent); 
